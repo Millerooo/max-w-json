ebd02ed2cdb2e1b176b4cb070574d4eb7af49df7
Millerooo <62337671+Millerooo@users.noreply.github.com>
2020-05-06 17:48:26 +0200

    Add files via upload

---
+++ maxJSON.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+int main ()
+{
+	HANDLE hOut;
+	
+	hOut = GetStdHandle( STD_OUTPUT_HANDLE );
+    int tablica[500];
+    
+ 
+    string linia;
+    fstream plik;
+ 
+    plik.open("a.txt", ios::in);
+    if(plik.good() == true)
+    {
+        while(!plik.eof())
+        {
+            getline(plik, linia);
+            cout << linia << endl;//wyœwietlenie linii
+        }
+        plik.close();
+	}                   ////////////////////1111/1///1/1///
+	{
+     SetConsoleTextAttribute( hOut, BACKGROUND_GREEN);
+	int max, tab[]={3, 5, 123, 2, 3, 4, 3, 2, 1, -1}; 
+	
+	max = tab[0]; //pierwsz¹ liczbê przypisujemy do zmiennej min
+	
+	for(int i=1;i<10;i++) //przeszukanie pozosta³ych 9 liczb
+		if(max<tab[i])
+		
+		
+			max = tab[i];
+	SetConsoleTextAttribute( hOut, BACKGROUND_RED);
+	cout<<"Najwieksza wczytana liczba jest "<<max<<endl;
+   
+   
+    ofstream plik2;              
+    plik2.open("b.txt",ios::out); 
+	plik2<<"[  \n { \n \"MAKSIMUM\" : \" ";       
+    plik2<<max ;
+	plik2<<endl;   
+	
+	 SetConsoleTextAttribute( hOut, BACKGROUND_GREEN);
+	cout<<endl<<"Wcisnij enter, aby zamknac program...";
+	
+	 SetConsoleTextAttribute( hOut, BACKGROUND_BLUE);
+	 cout<<endl<<"Mikolaj Miller";
+}
+			SetConsoleTextAttribute( hOut, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+       return 0; 
+    }
+
+
+
+
+
+
+
